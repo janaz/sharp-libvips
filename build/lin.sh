f0de841fbb1dc9eca6b251ec68e7942456ad6f68
Tomasz Janowski <janaz9@gmail.com>
2024-05-09 03:31:30 +0000

    bump glib

--- build/lin.sh
+++ build/lin.sh
@@ -94,7 +94,7 @@ CURL="curl --silent --location --retry 3 --retry-max-time 30"
 # Dependency version numbers
 VERSION_ZLIB_NG=2.1.6
 VERSION_FFI=3.4.6
-VERSION_GLIB=2.80.0
+VERSION_GLIB=2.80.1
 VERSION_XML2=2.12.6
 VERSION_EXIF=0.6.24
 VERSION_LCMS2=2.16
