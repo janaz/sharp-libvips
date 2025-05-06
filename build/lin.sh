75289eb4bdf57a4622bece9df0701583d0ccc3f0
Tomasz Janowski <janaz9@gmail.com>
2025-05-06 06:10:10 +0000

    try libvips-8.17.0-test4

--- build/lin.sh
+++ build/lin.sh
@@ -450,9 +450,9 @@ mkdir ${DEPS}/vips
 $CURL https://github.com/libvips/libvips/releases/download/v${VERSION_VIPS}/vips-${VERSION_VIPS}.tar.xz | tar xJC ${DEPS}/vips --strip-components=1
 cd ${DEPS}/vips
 # Use version number in SONAME
-$CURL https://gist.githubusercontent.com/lovell/313a6901e9db1bf285f2a1f1180499e4/raw/3988223c7dfa4d22745d9392034b0117abef1446/libvips-cpp-soversion.patch | patch -p1
+$CURL https://gist.githubusercontent.com/janaz/affeb831e648be8dd64f556007f88535/raw/1511be4a573284beec4eaec77c8a43ba7211ab21/libvips-cpp-soversion.patch | patch -p1
 # Disable HBR support in heifsave
-$CURL https://github.com/libvips/build-win64-mxe/raw/v${VERSION_VIPS}/build/patches/vips-8-heifsave-disable-hbr-support.patch | patch -p1
+$CURL https://gist.githubusercontent.com/janaz/affeb831e648be8dd64f556007f88535/raw/1511be4a573284beec4eaec77c8a43ba7211ab21/vips-8-heifsave-disable-hbr-support.patch | patch -p1
 # Link libvips.so statically into libvips-cpp.so
 sed -i'.bak' "s/library('vips'/static_&/" libvips/meson.build
 sed -i'.bak' "/version: library_version/{N;d;}" libvips/meson.build
